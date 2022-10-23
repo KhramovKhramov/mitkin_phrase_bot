7a4e84f32c8429ea80c01b52cae8ab933ffe911f
Antsiferov Bogdan <58101453+AntsiferovBogdan@users.noreply.github.com>
2022-10-23 12:39:31 +0200

    fake_headers added

--- parser/utils.py
+++ parser/utils.py
@@ -1,9 +1,8 @@
+from fake_headers import Headers
 import httpx
 
 def get_html(url):
-    headers =  {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:103.0) Gecko/20100101 Firefox/103.0'
-    }
+    headers = Headers(headers=True).generate()
     try:
         result = httpx.get(url, headers=headers)
         result.raise_for_status()
@@ -14,6 +13,4 @@ def get_html(url):
 
 def write_content(text):
     with open('titles.txt', 'a', encoding='utf-8') as f:
-        #f.write(f'{title}\n')
-        #f.write(anons)
         f.write(text)
